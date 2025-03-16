d66e8548e76a57b628615a455166e85f361de838
Elia Schneider <elyssch@gmail.com>
2025-03-16 02:50:31 +0100

    server init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config' //to autocomplete final import to.js
+import express from 'express'
+import cors from 'cors' //comunication to front
+import router from './router/router.js'
+
+
+
+
+const server = express() //server build it
+server.use(cors)
+server.use(express.json())
+server.use(router)
+
+
+server.listen(process.env.PORT, () => {
+    console.clear()
+    console.log('Hello - Server its running!')
+})
